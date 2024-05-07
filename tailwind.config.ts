255d5900a2dd40b4d1d8f983e3d16f139055c95c
bobby <rlatnguscjsw@gmail.com>
2024-05-08 00:05:46 +0900

    Feat : tailwind config
    -  Calc 사용을 위한 mode 설정
    
    Feat     : 새로운 기능 추가
    Refactor : 코드 리펙토링
    Fix      : 버그 수정
    Style    : CSS 수정
    Docs     : 문서 추가 및 수정
    Rename   : 폴더 이름 수정 및 위치 수정
    Remove   : 폴더 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  mode: "jit",
+  theme: {},
   plugins: [],
 };
 export default config;
